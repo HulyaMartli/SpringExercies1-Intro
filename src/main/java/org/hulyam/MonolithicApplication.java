48ef5a2398a301bf3ff2a1c893070fe10623996a
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-08-11 06:23:36 +0300

    main class name changed

--- src/main/java/org/hulyam/MonolithicApplication.java
+++ src/main/java/org/hulyam/MonolithicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExercies1MonolithicApplication {
+public class MonolithicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringExercies1MonolithicApplication.class, args);
+        SpringApplication.run(MonolithicApplication.class, args);
     }
 
 }
